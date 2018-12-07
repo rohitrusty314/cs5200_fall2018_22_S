ec3635914753efcac7ed162ed653332b3ea6e568
Himanshu Arora <harora22@ccs.neu.edu>
2018-12-07 15:31:49 -0500

    Moved to user controller

---
+++ movie-database-system/src/main/java/com/northeastern/edu/controllers/UserController.java
@@ -0,0 +1,39 @@
+package com.northeastern.edu.controllers;
+
+import com.northeastern.edu.models.Critic;
+import com.northeastern.edu.models.Resident;
+import com.northeastern.edu.models.User;
+import com.northeastern.edu.repositories.CriticRepository;
+import com.northeastern.edu.repositories.ResidentRepository;
+import com.northeastern.edu.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    CriticRepository criticRepository;
+
+    @Autowired
+    ResidentRepository residentRepository;
+
+
+    @GetMapping("/api/user/{id}")
+    public User getUser(@PathVariable("id") int id) {
+        return userRepository.findById(id).get();
+    }
+
+    @PostMapping("/api/resident")
+    public Resident createResident (@RequestBody Resident resident) {
+        return residentRepository.save(resident);
+    }
+
+    @PostMapping("/api/critic")
+    public Critic createCritic (@RequestBody Critic critic) {
+        return criticRepository.save(critic);
+    }
+}
