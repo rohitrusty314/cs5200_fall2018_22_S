c5e2d3654111ad067976a5dcbe0371feab4a546d
Himanshu Arora <harora22@ccs.neu.edu>
2018-12-07 17:48:06 -0500

    Model changed

---
+++ movie-database-system/src/main/java/com/northeastern/edu/models/MovieWatchlist.java
@@ -0,0 +1,65 @@
+package com.northeastern.edu.models;
+
+import javax.persistence.*;
+
+@Entity
+public class MovieWatchlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private boolean watched;
+
+    @ManyToOne
+    private Movie movie;
+
+    @ManyToOne
+    private Watchlist watchlist;
+
+    public MovieWatchlist() {
+
+    }
+
+    public MovieWatchlist(boolean watched) {
+        this.watched = watched;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isWatched() {
+        return watched;
+    }
+
+    public void setWatched(boolean watched) {
+        this.watched = watched;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+        if (!movie.getMovieWatchlists().contains(this)) {
+            movie.getMovieWatchlists().add(this);
+        }
+    }
+
+    public Watchlist getWatchlist() {
+        return watchlist;
+    }
+
+    public void setWatchlist(Watchlist watchlist) {
+        this.watchlist = watchlist;
+        if (!watchlist.getMovieWatchlists().contains(this)) {
+            watchlist.getMovieWatchlists().add(this);
+        }
+    }
+}
