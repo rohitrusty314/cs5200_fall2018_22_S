bb9f3444d22ab56e12cc5358d7a9ac5a0ec0b2ab
Rohit Raj <raj.r@husky.neu.edu>
2018-12-08 00:19:22 -0500

    final changes

--- movie-database-system/src/main/java/com/northeastern/edu/controllers/ReviewController.java
+++ movie-database-system/src/main/java/com/northeastern/edu/controllers/ReviewController.java
@@ -1,17 +1,9 @@
 package com.northeastern.edu.controllers;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.northeastern.edu.models.Critic;
-import com.northeastern.edu.models.CriticReview;
-import com.northeastern.edu.models.Movie;
-import com.northeastern.edu.models.Review;
+import com.northeastern.edu.models.*;
 import com.northeastern.edu.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
@@ -24,10 +16,16 @@ public class ReviewController {
     CriticReviewRepository criticReviewRepository;
 
     @Autowired
+    ResidentReviewRepository residentReviewRepository;
+
+    @Autowired
     MovieRepository movieRepository;
 
     @Autowired
     CriticRepository criticRepository;
+
+    @Autowired
+    ResidentRepository residentRepository;
 
     @PostMapping("/api/user/{userId}/movie/{movieId}/review/critic")
     public CriticReview createCriticReviewForMovie(@RequestBody CriticReview criticReview,
@@ -53,5 +51,51 @@ public class ReviewController {
             criticReview.setDownVotes(0);
         }
         return criticReviewRepository.save(criticReview);
+    }
+
+    @PostMapping("/api/user/{userId}/movie/{movieId}/review/resident")
+    public ResidentReview createResidentReviewForMovie(@RequestBody ResidentReview residentReview,
+                                                   @PathVariable("userId") int userId,
+                                                   @PathVariable("movieId") int movieId) {
+        Date today = java.util.Calendar.getInstance().getTime();
+        Review oldReview = reviewRepository.findByMovieIdAndUserId(movieId, userId);
+        if(oldReview != null) {
+            oldReview.setReview(residentReview.getReview());
+            oldReview.setUpdated(today);
+            residentReview = (ResidentReview) oldReview;
+        } else {
+            Resident resident = residentRepository.findById(userId).get();
+            residentReview.setUser(resident);
+
+            Movie movie = movieRepository.findById(movieId).get();
+            residentReview.setMovie(movie);
+
+
+            residentReview.setCreated(today);
+            residentReview.setUpdated(today);
+            residentReview.setEndorsedByCritic(false);
+        }
+        return residentReviewRepository.save(residentReview);
+    }
+
+    @PutMapping("/api/critic/review/{reviewId}/upvote")
+    public CriticReview upvoteCriticReview(@PathVariable("reviewId") int reviewId) {
+        CriticReview criticReview = criticReviewRepository.findById(reviewId).get();
+        criticReview.setUpVotes(criticReview.getUpVotes() + 1);
+        return criticReviewRepository.save(criticReview);
+    }
+
+    @PutMapping("/api/critic/review/{reviewId}/downvote")
+    public CriticReview downvoteCriticReview(@PathVariable("reviewId") int reviewId) {
+        CriticReview criticReview = criticReviewRepository.findById(reviewId).get();
+        criticReview.setDownVotes(criticReview.getDownVotes() + 1);
+        return criticReviewRepository.save(criticReview);
+    }
+
+    @PutMapping("/api/resident/review/{reviewId}/endorse")
+    public ResidentReview endorseResidentReview(@PathVariable("reviewId") int reviewId) {
+        ResidentReview residentReview = residentReviewRepository.findById(reviewId).get();
+        residentReview.setEndorsedByCritic(true);
+        return residentReviewRepository.save(residentReview);
     }
 }
--- movie-database-system/src/main/java/com/northeastern/edu/models/ResidentReview.java
+++ movie-database-system/src/main/java/com/northeastern/edu/models/ResidentReview.java
@@ -6,12 +6,10 @@ import java.util.Date;
 @Entity
 public class ResidentReview extends Review{
 
-
     private boolean endorsedByCritic;
 
-    public ResidentReview () {
+    public ResidentReview () { }
 
-    }
     public ResidentReview(String review, Date created, Date updated, boolean endorsedByCritic) {
         super(review, created, updated);
         this.endorsedByCritic = endorsedByCritic;
