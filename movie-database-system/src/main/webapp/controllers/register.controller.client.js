dbfcd63db4bfe0341dcd4d232c51090ab08400dd
Himanshu Arora <harora22@ccs.neu.edu>
2018-12-08 16:51:05 -0500

    Same error

--- movie-database-system/src/main/webapp/controllers/register.controller.client.js
+++ movie-database-system/src/main/webapp/controllers/register.controller.client.js
@@ -14,29 +14,22 @@
 
         function register(user) {
 
-            // console.log("reached in register controller")
-            // vm.alert = "";
-            // if (!user.email || user.email === undefined) {
-            //     vm.alert = "Invalid Email format";
-            // } else {
-            //     if (user.password === user.verifypassword) {
-            //         UserService
-            //             .findUserByUsername(user.username)
-            //             .then(function (response) {
-            //                 if (response === '') {
-            //                     UserService
-            //                         .createUser(user)
-            //                         .then(function (user) {
-            //                             $location.url("/login");
-            //                         })
-            //                 } else {
-            //                     vm.alert = "User Name already taken, Please try another one";
-            //                 }
-            //             })
-            //     } else {
-            //         vm.alert = "Password and verify password do not match";
-            //     }
-            // }
+            console.log("reached in register controller")
+            vm.alert = "";
+            if (!user.email || user.email === undefined) {
+                vm.alert = "Invalid Email format";
+            } else {
+                if (user.password === user.verifypassword) {
+                    UserService
+                        .createUser(user)
+                        .then(function (user) {
+                            $location.url("/login");
+                        })
+
+                } else {
+                    vm.alert = "Password and verify password do not match";
+                }
+            }
         }
     }
 })();
--- movie-database-system/src/main/webapp/services/user.service.client.js
+++ movie-database-system/src/main/webapp/services/user.service.client.js
@@ -3,25 +3,26 @@
         .module("MovieDBApp")
         .factory("UserService", userService);
 
+
     function userService($http) {
-        // var api = {
-        //     "findUserByCredentials": findUserByCredentials,
-        //     "createUser": createUser
-        // };
-        // return api;
-        //
-        // function findUserByCredentials(user) {
-        //     return $http.post("/api/login", user)
-        //         .then(function (response) {
-        //             return response.data;
-        //         });
-        // }
-        //
-        // function createUser(user) {
-        //     return $http.post("/api/user", user)
-        //         .then(function (response) {
-        //             return response.data;
-        //         });
-        // }
+        var api = {
+            "findUserByCredentials": findUserByCredentials,
+            "createUser": createUser
+        };
+        return api;
+
+        function findUserByCredentials(user) {
+            return $http.post("/api/login", user)
+                .then(function (response) {
+                    return response.data;
+                });
+        }
+
+        function createUser(user) {
+            return $http.post("/api/user", user)
+                .then(function (response) {
+                    return response.data;
+                });
+        }
     }
 })();
