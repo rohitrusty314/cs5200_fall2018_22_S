c1a1ca4fb90dbc80cd42b0d9b7d88c67a2f7e702
Rohit Raj <raj.r@husky.neu.edu>
2018-12-10 23:10:28 -0500

    application file extends

--- movie-database-system/src/main/java/com/northeastern/edu/MovieDatabaseSystemApplication.java
+++ movie-database-system/src/main/java/com/northeastern/edu/MovieDatabaseSystemApplication.java
@@ -2,10 +2,19 @@ package com.northeastern.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MovieDatabaseSystemApplication {
+public class MovieDatabaseSystemApplication   extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder
+    configure(SpringApplicationBuilder application) {
+        return application.sources(
+                MovieDatabaseSystemApplication.class);
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(MovieDatabaseSystemApplication.class, args);
     }
